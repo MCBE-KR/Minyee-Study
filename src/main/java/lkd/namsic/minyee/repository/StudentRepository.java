5c48c85a4ac4733d71bd2a6cc04effa91705a430
minyee2913 <leegm0759@gmail.com>
2022-04-06 23:57:16 +0900

    initial commit

--- src/main/java/lkd/namsic/minyee/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package lkd.namsic.minyee.repository;
-
-import lkd.namsic.minyee.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
